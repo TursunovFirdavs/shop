ac7dc5a2dce6c197daf7f530901dbacb9121a68d
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-01-31 17:40:44 +0500

    handle increment and decrement

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -2,11 +2,57 @@
 
 import CustomImage from '@/components/image';
 import { ProductTypes } from '@/interface'
+import { useState } from 'react';
 import { FaStar, FaRegStar } from 'react-icons/fa';
 
 const ShoppingCart = () => {
 
-  const products: ProductTypes[] = JSON.parse(localStorage.getItem('carts') as string) || []
+  const [products, setProducts] = useState<ProductTypes[]>(
+    JSON.parse(localStorage.getItem('carts') as string) || []
+  )
+
+  const removeProduct = (id: number) => {
+    console.log(id);
+    const updatedCart = products.filter(product => product.id !== id)
+    localStorage.setItem('carts', JSON.stringify(updatedCart))
+    setProducts(updatedCart)
+  }
+
+  const handleIncrement = (id: number) => {
+    const updatedCart = products.map(product => {
+      if(product.id === id) {
+        return {
+          ...product,
+          quantity: product.quantity + 1
+        }
+      }
+      return product
+    })
+
+    localStorage.setItem('carts', JSON.stringify(updatedCart))
+    setProducts(updatedCart)
+  }
+
+  const handleDecrement = (id: number) => {
+    const existProduct = products.find(product => product.id === id)
+    if(existProduct?.quantity === 1) {
+      removeProduct(existProduct.id)
+    }
+    else {
+      const updatedCart = products.map(product => {
+        if(product.id === id) {
+          return {
+            ...product,
+            quantity: product.quantity - 1
+          }
+        }
+        return product
+      })
+      localStorage.setItem('carts', JSON.stringify(updatedCart))
+      setProducts(updatedCart)
+    }
+
+  }
 
   return (
     <div className="h-screen bg-gray-100 pt-20">
@@ -59,13 +105,13 @@ const ShoppingCart = () => {
                   </div>
                   <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
                     <div className="flex items-center border-gray-100">
-                      <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                      <span onClick={() => handleDecrement(c.id)} className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
                       <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={c.quantity} min="1" />
-                      <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                      <span onClick={() => handleIncrement(c.id)} className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
                     </div>
                     <div className="flex items-center space-x-4">
                       <p className="text-sm ">{c.price.toLocaleString('en-US', {style: 'currency', currency: 'usd'})}</p>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
+                      <svg onClick={() => removeProduct(c.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                       </svg>
                     </div>
